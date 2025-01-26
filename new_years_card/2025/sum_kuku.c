944766cd4e894efa64c6556a23dd241c9a439494
溝口洸熙 <94270819+MIZOGUCHIKoki@users.noreply.github.com>
2025-01-26 15:21:25 +0900

    Update sum_kuku.c

--- new_years_card/2025/sum_kuku.c
+++ new_years_card/2025/sum_kuku.c
@@ -3,6 +3,6 @@ int main() {
  int i; int j; int ans = 0;
  for (i = 1; i <= 9; i++){ for (j = 1; j <= 9; j++) {
 	 ans += j * i; } }
-	printf("%d\n", ans); // 25
+	printf("%d\n", ans); // 2025
 	return 0;
 }
